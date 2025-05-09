15e598ef26584ef035785d2586fb5a52f28a9ea7
klayhung <klayhung@gmail.com>
2025-05-09 09:26:41 +0800

    變更 Identity 表格名稱 (aps_xxx -> aps_xxx)

--- DevHabit/DevHabit.Api/Database/ApplicationIdentityDbContext.cs
+++ DevHabit/DevHabit.Api/Database/ApplicationIdentityDbContext.cs
@@ -16,13 +16,13 @@ public sealed class ApplicationIdentityDbContext(DbContextOptions<ApplicationIde
 
         builder.HasDefaultSchema(Schema.Identity);
 
-        builder.Entity<IdentityUser>().ToTable("aps_net_users");
-        builder.Entity<IdentityRole>().ToTable("aps_net_roles");
-        builder.Entity<IdentityUserRole<string>>().ToTable("aps_net_user_roles");
-        builder.Entity<IdentityUserClaim<string>>().ToTable("aps_net_user_claims");
-        builder.Entity<IdentityRoleClaim<string>>().ToTable("aps_net_role_claims");
-        builder.Entity<IdentityUserLogin<string>>().ToTable("aps_net_user_logins");
-        builder.Entity<IdentityUserToken<string>>().ToTable("aps_net_user_tokens");
+        builder.Entity<IdentityUser>().ToTable("asp_net_users");
+        builder.Entity<IdentityRole>().ToTable("asp_net_roles");
+        builder.Entity<IdentityUserRole<string>>().ToTable("asp_net_user_roles");
+        builder.Entity<IdentityUserClaim<string>>().ToTable("asp_net_user_claims");
+        builder.Entity<IdentityRoleClaim<string>>().ToTable("asp_net_role_claims");
+        builder.Entity<IdentityUserLogin<string>>().ToTable("asp_net_user_logins");
+        builder.Entity<IdentityUserToken<string>>().ToTable("asp_net_user_tokens");
 
         builder.Entity<RefreshToken>(entity =>
         {
