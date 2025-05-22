d027c97c6c4ecd8309b1186821f2b40c0a00e968
klayhung <klayhung@gmail.com>
2025-05-22 16:17:28 +0800

    修正 Audience 驗證方式，使用 Auth0 會有多組數值

--- DevHabit/DevHabit.Api/DependencyInjection.cs
+++ DevHabit/DevHabit.Api/DependencyInjection.cs
@@ -282,7 +282,15 @@ public static class DependencyInjection
                 if (jwtAuthOptions.ExternalAuth)
                 {
                     options.Authority = jwtAuthOptions.Authority;
-                    options.Audience = jwtAuthOptions.Audience;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = jwtAuthOptions.Authority,
+
+                        ValidateAudience = true,
+                        AudienceValidator = (audiences, _, _) =>
+                            audiences != null && audiences.Contains(jwtAuthOptions.Audience),
+                    };
                 }
                 else
                 {
